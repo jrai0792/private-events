1388996b4b79432040c2ed9a5329f2e058213b5d
Jyoti <jyotikiran12@gmail.com>
2020-12-06 22:30:28 +0530

    Fixed linters error.

--- spec/model/invitation_spec.rb
+++ spec/model/invitation_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Invitation, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   # describe 'Associations' do
-    it { should belong_to(:event) }
-    it { should belong_to(:invitor) }
-    it { should belong_to(:invitee) }
+  it { should belong_to(:event) }
+  it { should belong_to(:invitor) }
+  it { should belong_to(:invitee) }
   # end
 end
